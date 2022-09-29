ce57ce90d8c1df1338ca3d6d85283eb806859dbf
Johan Dorcas <johandorcas99@gmail.com>
2022-09-29 14:08:54 +0200

    gevulde repository

---
+++ #condities.py
@@ -0,0 +1,10 @@
+#condities
+rekenen = 3 #rekenen altijd voldoende
+engels = 4 #talen minimaal 1 voldoende voor engels en NL
+nederlands = 5
+
+# and/ or en? ()
+if  rekenen > 5 and engels > 5 or nederlands > 5: #voor alles een voldoende
+     print("Je mag naar leerjaar 2")
+else: 
+     print("Helaas je blijft zitten")
---
+++ Hallowereld.py
@@ -0,0 +1,4 @@
+from select import KQ_FILTER_AIO
+
+
+Hello_wereld_van =print("ppp   y    y   tttt   h    h    ooooo     nnnnn \np p   y   y      t    h    h    o    o    n    n \np p    y  y      t    hhhhhh    o    o    n    n    \np       y        t    h    h    o    o    n    n \np       y        t    h    h    ooooo     n    n ")
