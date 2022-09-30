e137a318b4b70ce570496c8c237551d4981e5140
Johan Dorcas <johandorcas99@gmail.com>
2022-09-30 09:38:44 +0200

    Nieuwe versie

--- module02OmgaanMet/feestlunch.py
+++ module02OmgaanMet/feestlunch.py
@@ -4,5 +4,3 @@ stokbroden_in_totaal = 5.56
 totaal = croissantjes_in_totaal + stokbroden_in_totaal
 
 print("De feestlunch kost je bij de bakker", round(totaal/2 * 3,2), "euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
-
-round(totaal/2 * 3, 2)
