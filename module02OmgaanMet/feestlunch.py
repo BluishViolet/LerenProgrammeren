b52eb03e0777ac4836ae92ed48f74ec2820c0773
Johan Dorcas <johandorcas99@gmail.com>
2022-10-04 09:42:18 +0200

    Nieuwe versie

--- module02OmgaanMet/feestlunch.py
+++ module02OmgaanMet/feestlunch.py
@@ -1,6 +1,14 @@
-croissantjes_in_totaal = 6.63
-stokbroden_in_totaal = 5.56
+aantal_croissantjes = 17
+prijs_croissantjes = 0.39
+aantal_stokbroden = 2
+prijs_stokbroden = 2.78
+kortingsbonnen = 2
+aantal_kortingsbonnen = 3
+
+
+croissantjes_in_totaal = aantal_croissantjes * prijs_croissantjes
+stokbroden_in_totaal = aantal_stokbroden * prijs_stokbroden
 
 totaal = croissantjes_in_totaal + stokbroden_in_totaal
 
-print("De feestlunch kost je bij de bakker", round(totaal/2 * 3,2), "euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
+print("De feestlunch kost je bij de bakker", round(totaal/kortingsbonnen * aantal_kortingsbonnen ,2), "euro voor de" ,aantal_croissantjes, "croissantjes en de", aantal_stokbroden, "stokbroden als de", aantal_kortingsbonnen, "kortingsbonnen nog geldig zijn!")
--- module02OmgaanMet/speelhal.py
+++ module02OmgaanMet/speelhal.py
@@ -1,6 +1,7 @@
-toegangsticket = 7.45
-gameseatperpersoon = 0.37
-minuten = 5
-personen = 4
+toegangsticketperpersoon = 7.45 * 4
+gameseatpermin = 0.37 * 5
+minuten = 45
+mensen = 4
 
-print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar:",round(toegangsticket + gameseatperpersoon * minuten * personen,2) , "euro")
+print("Dit geweldige dagje-uit met" ,mensen, "personen  in de Speelhal met 45 minuten VR kost je maar:",round(toegangsticketperpersoon + gameseatpermin + minuten/mensen,2) , "euro")
+
