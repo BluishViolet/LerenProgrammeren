1ccfccacd3c3d3ccf9633a518fcb3ebceb69bd63
Johan Dorcas <johandorcas99@gmail.com>
2022-10-04 13:10:02 +0200

    person

---
+++ module02OmgaanMet/person.py
@@ -0,0 +1,7 @@
+naam = input('Uw Naam:');
+adress = input('Uw Adress:');
+postcode = input('Uw Postcode:');
+woonplaats = input(' Uw Woonplaats:');
+
+
+print("---------------------------------------\n|  Naam      : ",naam, "\n|  Adress    : ", adress, "\n|  Postcode  : ", postcode, "\n|  Woonplaats: ", woonplaats, "\n----------------------------------------")
--- module02OmgaanMet/speelhal.py
+++ module02OmgaanMet/speelhal.py
@@ -1,7 +1,7 @@
-toegangsticketperpersoon = 7.45 * 4
-gameseatpermin = 0.37 * 5
+toegangsticket = 7.45 
+gameseatpermin = 0.37 / 5
 minuten = 45
 mensen = 4
-
-print("Dit geweldige dagje-uit met" ,mensen, "personen  in de Speelhal met 45 minuten VR kost je maar:",round(toegangsticketperpersoon + gameseatpermin + minuten/mensen,2) , "euro")
-
+toegangsticket_totaal = toegangsticket * mensen
+vrTotaal = gameseatpermin * minuten
+print("Dit geweldige dagje-uit met" ,mensen, "personen in de Speelhal met", minuten, "minuten VR kost je maar:",round(toegangsticket_totaal + gameseatpermin + vrTotaal,2) , "euro")
