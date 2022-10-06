7bea1cb9d71ea8100bce12f08e8b79760d6b484c
Johan Dorcas <johandorcas99@gmail.com>
2022-10-06 11:32:56 +0200

    MinMax

--- module02OmgaanMet/MinMax.py
+++ module02OmgaanMet/MinMax.py
@@ -1,9 +1,15 @@
-from re import A
-
-
 a = 10
 b = 5
 
 if a > b:
     Max = a
-    print("a is het grootste getal:",Max)
+    print( f"a is het grootste getal:{Max}")
+elif a < b:
+    Min = b
+    print(f"b is het kleinste getal:, {Min}")
+else:
+    print("a en b zijn even groot")
+
+
+print("Het minimum is: b gevolgd door de waarde van Min")
+print("Het maximum is: a gevolgd door de waarde van Max")
