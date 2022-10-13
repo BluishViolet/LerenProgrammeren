ccd25217f87ddc2e2b39177f7d8fae7b72120d74
Johan Dorcas <johandorcas99@gmail.com>
2022-10-13 10:02:49 +0200

    stap 3

--- module02OmgaanMet/zwembad3.py
+++ module02OmgaanMet/zwembad3.py
@@ -7,9 +7,13 @@ uitgravenTotaal = afvoerendGrond * uitgravenpPrijs
 vastePrijs = 32.50
 prijsPerM3 = afvoerendGrond * vastePrijs
 inTotaal = uitgravenTotaal + afvoerendGrond
+voorijkostenPerKM = 2.05
+afstandBedrijfKM = 60
+voorijkostenPerkmTotaal = voorijkostenAfstand * afstandBedrijfKM
 
 print(f'''Offerte voor een zwembad van 8 bij 1,5 meter
 Uitgraven:        €{uitgravenTotaal}
 Afvoeren grond:   €{afvoerendGrond}
+Voorijkosten:     €{voorijkostenPerkmTotaal} 
 Totaal:           €{inTotaal}  
 ''')
